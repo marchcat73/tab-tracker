5fc9b703c44d2066854435b4044273161541f4b8
unknown <D:\Program Files (x86)\The Bat!\Mail>
2018-12-21 20:29:16 +0400

    create register

--- server/src/app.js
+++ server/src/app.js
@@ -16,4 +16,10 @@ app.get('/status', (req, res) => {
 	})
 })
 
+app.post('/register', (req, res) => {
+	res.send({
+		message: `hello ${req.body.email} register`
+	})
+})
+
 app.listen(process.env.PORT || 8081)
